2be26646daf473c2e2f35a9f0a6ccf67fde7e86d
beginerdev1100 <CCH5509nance@gmail.com>
2025-06-01 14:29:57 -0500

    Add files via upload

---
+++ RPS.py
@@ -0,0 +1,29 @@
+i = ["Rock!", "Paper!", "Sissors!"]
+
+def main():
+    print("Ok lets start!")
+    start = input("Chose rock, paper, or sissors. ").lower().strip()
+    if start == ("rock")
+        rock()
+
+
+def rock()
+    if i == ("Paper"):
+        print("You loose!")
+
+    elif i == ("sissors")
+        print("You win!")
+
+    elif i == ("rock")
+        print("Tie!")
+
+
+def start():
+    s = input("Do u want to play rock paper siserse shoot! ").lower().strip()
+
+    if s == ("yes"):
+        main()
+    else:
+        ("Mabby another time")
+
+start()
